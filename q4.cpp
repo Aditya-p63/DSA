708818cf35c7ae5392442673d0ee83629454846b
Aditya raj prasad <prasad987aditya@gmail.com>
2026-02-20 18:48:34 +0530

    Add files via upload

---
+++ q4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout << "enter first  number";
+    cin >> x;
+    int y;
+    cout << "enter seecond  number";
+    cin >> y;
+    int z;
+    cout << "enter third  number";
+    cin >> z; 
+    
+    return 0;
+}
