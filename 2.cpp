87e0a385eada733577ea1eeb63c82c33d401b079
Aditya raj prasad <prasad987aditya@gmail.com>
2026-02-24 19:52:33 +0530

    Add files via upload

--- 2.cpp
+++ 2.cpp
@@ -1,32 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int x;
-    cin>>x;
-    switch (x){
-        case 1 :
-        cout<<"monday";
-        break;
-         case 2 :
-        cout<<"tuesday";
-        break;
-         case 3 :
-        cout<<"wednesday";
-        break;
-         case 4 :
-        cout<<"thrusday";
-        break;
-         case 5 :
-        cout<<"friday";
-        break;
-         case 6 :
-        cout<<"saturday";
-        break;
-         case 7 :
-        cout<<"sunday";
-        break;
-        default : cout<<"invalid";
 
+    int i=97;
+    while(i<=122){
+        cout<<(char)i<<" "<<i<<endl;
+        i++;
     }
-    
+    return 0;
 }
