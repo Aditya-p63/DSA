51ef6da2afac1c80a5323adfa903f4c670a4b4be
Aditya raj prasad <prasad987aditya@gmail.com>
2026-02-24 18:26:08 +0530

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
 using namespace std;
-int main()
-   { int x;
-    cin>>x;
-    
-
-    x>33 ? cout<<"pass" : cout<<"fail";
+int main(){
+    int n;
+    cout<<"enter the term : ";
+    cin>>n;
+    int a =4;
+    for(int i=1;i<=n;i++){
+        cout<<a<<endl;
+        a=a+3;
+    }
     return 0;
-   }
+}
