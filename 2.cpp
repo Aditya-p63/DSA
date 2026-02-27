13c6b87db0a8fc98e0b250846224c335f2d897d8
Aditya raj prasad <prasad987aditya@gmail.com>
2026-02-27 22:57:59 +0530

    Add files via upload

--- 2.cpp
+++ 2.cpp
@@ -1,21 +1,17 @@
-// WAP to print the sum of all the even digits of a given number.
-
-// Sample Input : 4556
-// Output: 10
 #include<iostream>
-using namespace std;
+using namespace  std;
 int main(){
     int n;
-    cout<<"enter a number"<<endl;
     cin>>n;
-    int last;
-    int x=n;
-    int sum=0;
-    while(n>0){
-        last=n%10;
-         n=n/10;
-        if(last%2==0) {sum = sum+last;}
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
-    cout<<sum;
-    return 0;
+    int max=arr[0];
+    //cin>>x;
+    for(int i=1;i<n;i++){
+      //  sum=sum+arr[i];
+      if(max<arr[i]) max=arr[i];
+    }
+ cout<<max;
 }
