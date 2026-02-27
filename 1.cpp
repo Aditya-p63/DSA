abbe558337fa17f67c31de8eb3055e36be750c07
Aditya raj prasad <prasad987aditya@gmail.com>
2026-02-27 22:58:24 +0530

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,15 +1,28 @@
 #include<iostream>
-using namespace  std;
-int main(){
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> a;
     int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    cout << "Enter " << n << " elements:" << endl;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        a.push_back(x);
     }
-    int sum=0;
-    for(int i=0;i<n;i++){
-        sum=sum+arr[i];
+
+   vector<int> a1(n);
+   for(int i=0;i<n;i++){
+    for(int j=n-1;j>=0;j--){
+        a1[i]=a[j];
     }
- cout<<sum;
+   }
+   for(int i=0;i<n;i++){
+    cout<<a1[i];
+   }
 }
+
