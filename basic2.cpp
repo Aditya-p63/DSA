e1978357c37c1fe0f821cd97bbc50ba426800a83
Aditya raj prasad <prasad987aditya@gmail.com>
2026-02-28 23:15:17 +0530

    Add files via upload

---
+++ basic2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+};
+int main(){
+    Node a(10);
+    Node b(10);
+    a.next=&b;
+     cout<<(a.next)->val;
+    cout<<endl<<a.next;
+}
